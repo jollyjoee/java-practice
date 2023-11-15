51b2b771a41fd379e8cc781f114eb60878ff3056
jollyjoee <pisk.julijo@gmail.com>
2023-11-15 20:19:52 +0800

    explanation of formula

--- scripts/5per.js
+++ scripts/5per.js
@@ -21,14 +21,14 @@ function logic(){
       return parseFloat(x, 10);
   });
   
-  let paymentLogic = paymentArray.reduce(sum); //adds all expenses
+  let groupTotal = paymentArray.reduce(sum); //adds all expenses
   function sum(total, num) {
       return total + num
   };
-  const division = paymentLogic / 5 //divides all expenses by 5
+  const perMemCost = groupTotal / 5 //divides all expenses by 5
 
   const contrib = paymentArray.map(function(element){ //rounds each index to the nearest hundredths
-      return Math.round((element - division) * 100) / 100;
+      return Math.round((element - perMemCost) * 100) / 100;
    });
   
   //let contrib = [-31.8, 18.2, 168.2, -222.8, 68.2]
@@ -60,9 +60,10 @@ function logic(){
   let negativeList = negPer.toString() 
   negativeList = negativeList.replaceAll(',', '<br>')
   negativeList = negativeList.concat('<br>')
-  positivePer = positiveIndices.join(', ')
+  positivePer = positiveIndices.join('<br>')
   negativeTotal = Math.abs(Math.round(negativeTotal * 1000) / 1000);
-  let finalOutput = negativeList.concat(' ', 'The persons listed above should combine their assigned costs amounting to:', ' ', negativeTotal, ' ', 'and give ', ' ', positivePer.toString())
+  let startString = 'The total group expense is (Sum of all expenses):';
+  let finalOutput = startString.concat(' ', groupTotal, '<br><br> Each member\'s equal cost (x) is calculated by adding all expenses and dividing it by the number of people (Group\'s Total/5): ', perMemCost, '<br><br>', 'Each person\'s contribution is calculated by: Amount Spent = y => (y-x) (negative value people give positive value people their contribution) which are listed below:<br>', negativeList, '<br>The persons listed above should combine their contributions amounting to:', ' ', negativeTotal, ' ', 'and give:<br>', ' ', positivePer.toString())
 
   console.log(negativeList)
   /*
